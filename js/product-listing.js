21dbd10f35303c30ce47aeacb5722d6e9c7e846c
thomasgohard <thomas@gohard-soutar.com>
2013-09-05 17:26:09 -0400

    Began adding functionality to create equal-height columns on each row when the product listing is displayed using multiple columns on larger screens.

--- js/product-listing.js
+++ js/product-listing.js
@@ -36,6 +36,9 @@ pe.document.one('wb-init-loaded', function() {
 			},
 			{
 				"mData": function(data, type, val) {
+					/*
+					 * Bug: Assume that more than one sentence will be present in the product description.
+					 */
 					return data["ProductDescription"].substr(0, data["ProductDescription"].indexOf(". "));
 				},
 				"sClass": "product-description",
@@ -44,6 +47,33 @@ pe.document.one('wb-init-loaded', function() {
 		],
 		"fnCreatedRow": function(row, data, index) {
 			row.className = "product-record module-info module-simplify";
+		},
+		"fnDrawCallback": function(settings) {
+			var mq768 = window.matchMedia("(min-width: 768px)");
+			//mq768.addListener(event_handler);
+			//event_handler(mq768);
+
+			if (mq768.matches) {
+				var rows = this.fnGetNodes();
+				var recs_per_row = 2;
+
+				for (var i = 0, len = rows.length, minheight = 0; i < len; ++i) {
+					if (i % recs_per_row == 0) {
+						if (i !== 0) {
+							for (var j = i - 1; j >= i - recs_per_row; --j) {
+								rows[j].css({"min-height": minheight});
+							}
+						}
+
+						minheight = rows[i].height();
+					} else if (rows[i].height() > minheight) {
+						minheight = rows[i].height();
+					}
+				}
+			}
+			/*$.getScript('./wet-boew-dist/dist/js/dependencies/equalheights-min.js', function(data, textStatus, jqxhr) {
+				$('#mobile-gallery tbody').equalHeights();
+			});*/
 		}
 	});
 });
